2b6e4d21b4ab74284ed427a7f9e9463f15b8efea
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-03-09 14:38:06 +0300

    added endpoint direction enum

--- mtp/usb/Interface.h
+++ mtp/usb/Interface.h
@@ -5,6 +5,9 @@
 
 namespace mtp { namespace usb
 {
+	enum struct EndpointDirection {
+		In, Out
+	};
 	class Endpoint
 	{
 		const libusb_endpoint_descriptor & _endpoint;
@@ -14,6 +17,15 @@ namespace mtp { namespace usb
 
 		u8 GetAddress() const
 		{ return _endpoint.bEndpointAddress; }
+
+		EndpointDirection GetDirection() const
+		{
+			u8 dir = GetAddress() & LIBUSB_ENDPOINT_DIR_MASK;
+			if (dir == LIBUSB_ENDPOINT_IN)
+				return EndpointDirection::In;
+			else
+				return EndpointDirection::Out;
+		}
 	};
 	DECLARE_PTR(Endpoint);
 
