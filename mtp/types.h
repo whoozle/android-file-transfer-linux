ea9f0469a2dba0a4fb037cf303e342bf99a85070
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-03-13 22:57:06 +0300

    put types into namespace

--- mtp/types.h
+++ mtp/types.h
@@ -8,41 +8,44 @@
 #include <errno.h>
 #include <string.h>
 
-typedef std::unique_lock<std::mutex> scoped_mutex_lock;
+namespace mtp
+{
+	typedef std::unique_lock<std::mutex> scoped_mutex_lock;
 
-typedef uint8_t		u8;
-typedef uint16_t	u16;
-typedef uint32_t	u32;
-typedef uint64_t	u64;
+	typedef uint8_t		u8;
+	typedef uint16_t	u16;
+	typedef uint32_t	u32;
+	typedef uint64_t	u64;
 
-typedef int8_t		s8;
-typedef int16_t		s16;
-typedef int32_t		s32;
-typedef int64_t		s64;
+	typedef int8_t		s8;
+	typedef int16_t		s16;
+	typedef int32_t		s32;
+	typedef int64_t		s64;
 
-#define DECLARE_PTR(C) typedef std::shared_ptr<C> C##Ptr
+	#define DECLARE_PTR(C) typedef std::shared_ptr<C> C##Ptr
 
-template<typename T>
-inline T RequireNotNull(const T &t, const std::string &expr) {
-	if (!t)
-		throw std::runtime_error("null check failed " + expr);
-	return t;
+	template<typename T>
+	inline T RequireNotNull(const T &t, const std::string &expr) {
+		if (!t)
+			throw std::runtime_error("null check failed " + expr);
+		return t;
+	}
+
+	struct Noncopyable
+	{
+		Noncopyable() = default;
+		Noncopyable(const Noncopyable&) = delete;
+		Noncopyable& operator=(const Noncopyable &) = delete;
+	};
+
+	class system_error : public std::exception
+	{
+		std::string _message;
+	public:
+		system_error(const std::string &prefix, int err = 0) : _message(prefix + ": " + strerror(err? err: errno)){ }
+		virtual const char * what() const throw() { return _message.c_str(); }
+	};
 }
-
-struct Noncopyable
-{
-	Noncopyable() = default;
-	Noncopyable(const Noncopyable&) = delete;
-	Noncopyable& operator=(const Noncopyable &) = delete;
-};
-
-class system_error : public std::exception
-{
-	std::string _message;
-public:
-	system_error(const std::string &prefix, int err = 0) : _message(prefix + ": " + strerror(err? err: errno)){ }
-	virtual const char * what() const throw() { return _message.c_str(); }
-};
 
 #define NOT_NULL(x) (RequireNotNull((x), #x))
 
