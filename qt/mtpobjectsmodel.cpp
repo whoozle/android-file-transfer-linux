a9fc97d00333b944618aa0b986220c820235f234
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-03-13 23:10:12 +0300

    added reading/caching object info

--- qt/mtpobjectsmodel.cpp
+++ qt/mtpobjectsmodel.cpp
@@ -11,20 +11,59 @@ MtpObjectsModel::~MtpObjectsModel()
 
 }
 
+void MtpObjectsModel::setParent(mtp::u32 parentObjectId)
+{
+	beginResetModel();
+
+	_parentObjectId = parentObjectId;
+	mtp::msg::ObjectHandles handles = _session->GetObjectHandles(mtp::Session::AllStorages, (mtp::u32)mtp::ObjectFormat::Association, parentObjectId);
+	_rows.clear();
+	_rows.reserve(handles.ObjectHandles.size());
+	for(size_t i = 0; i < handles.ObjectHandles.size(); ++i)
+	{
+		mtp::u32 oid = handles.ObjectHandles[i];
+		_rows.append(Row(oid));
+	}
+
+	endResetModel();
+}
+
 void MtpObjectsModel::setSession(mtp::SessionPtr session)
 {
 	beginResetModel();
 	_session = session;
+	setParent(mtp::Session::Root);
 	endResetModel();
 }
 
 int MtpObjectsModel::rowCount(const QModelIndex &parent) const
-{
-	qDebug() << "ROW COUNT";
-	return 0;
-}
+{ return _rows.size(); }
 
 QVariant MtpObjectsModel::data(const QModelIndex &index, int role) const
 {
+	int row_idx = index.row();
+	if (row_idx < 0 || row_idx > _rows.size())
+		return QVariant();
+
+	switch(role)
+	{
+	case Qt::DisplayRole:
+		{
+			Row &row = _rows[row_idx];
+			if (!row.Info)
+			{
+				row.Info = std::make_shared<mtp::msg::ObjectInfo>();
+				try
+				{
+					*row.Info = _session->GetObjectInfo(row.ObjectId);
+					//qDebug() << QString::fromUtf8(row.Info->Filename.c_str());
+				}
+				catch(const std::exception &ex)
+				{ qDebug() << "failed to get object info " << ex.what(); }
+			}
+			return QString::fromUtf8(row.Info->Filename.c_str());
+		}
+		break;
+	}
 	return QVariant();
 }
--- qt/mtpobjectsmodel.h
+++ qt/mtpobjectsmodel.h
@@ -3,16 +3,28 @@
 
 #include <qabstractitemmodel.h>
 #include <mtp/ptp/Device.h>
+#include <QVector>
 
 class MtpObjectsModel : public QAbstractListModel
 {
 	mtp::SessionPtr		_session;
+	mtp::u32			_parentObjectId;
+
+	struct Row
+	{
+		mtp::u32								ObjectId;
+		std::shared_ptr<mtp::msg::ObjectInfo>	Info;
+		Row(mtp::u32 id = 0): ObjectId(id) { }
+	};
+
+	mutable QVector<Row>		_rows;
 
 public:
 	MtpObjectsModel(QObject *parent = 0);
 	~MtpObjectsModel();
 
 	void setSession(mtp::SessionPtr session);
+	void setParent(mtp::u32 parentObjectId);
 
 	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
 	virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
