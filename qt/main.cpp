eaf4e00f93f815372997a9f7d4b073bc4e79b8e5
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-03-17 01:33:41 +0300

    reimplemented notify

--- qt/main.cpp
+++ qt/main.cpp
@@ -1,5 +1,26 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QMessageBox>
+
+namespace
+{
+	class Application : public QApplication
+	{
+		Application(int &argc, char **argv, int flags = ApplicationFlags):
+			QApplication(argc, argv, flags)
+		{ }
+
+		virtual bool notify ( QObject * receiver, QEvent * e )
+		{
+			try {
+				return QApplication::notify( receiver, e );
+			} catch ( const std::exception& e ) {
+				QMessageBox::warning(0, "Error", e.what());
+				return false;
+			}
+		}
+	};
+}
 
 int main(int argc, char *argv[])
 {
