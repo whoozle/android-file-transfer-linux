39877a59ff19cf224828d5b7749323f1b5590c2e
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-04-12 23:05:06 +0300

    ported libusb backend

--- mtp/backend/libusb/usb/Device.cpp
+++ mtp/backend/libusb/usb/Device.cpp
@@ -77,13 +77,16 @@ namespace mtp { namespace usb
 			throw std::runtime_error("short write");
 	}
 
-	bool Device::ReadBulk(const EndpointPtr & ep, const IObjectOutputStreamPtr &outputStream, int timeout)
+	void Device::ReadBulk(const EndpointPtr & ep, const IObjectOutputStreamPtr &outputStream, int timeout)
 	{
 		ByteArray data(ep->GetMaxPacketSize() * 1024);
-		int tr = 0;
-		USB_CALL(libusb_bulk_transfer(_handle, ep->GetAddress(), data.data(), data.size(), &tr, timeout));
-		outputStream->Write(data.data(), tr);
-		return tr == (int)data.size();
+		int tr;
+		do
+		{
+			USB_CALL(libusb_bulk_transfer(_handle, ep->GetAddress(), data.data(), data.size(), &tr, timeout));
+			outputStream->Write(data.data(), tr);
+		}
+		while(tr == (int)data.size());
 	}
 
 
