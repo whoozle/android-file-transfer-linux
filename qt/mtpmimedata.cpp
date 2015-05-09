b63139bfd2700ab93d9247425868ea7247e4373c
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-05-09 12:35:14 +0300

    added missing licenses

---
+++ qt/mtpmimedata.cpp
@@ -0,0 +1,7 @@
+#include "mtpmimedata.h"
+
+MtpMimeData::MtpMimeData()
+{
+
+}
+
---
+++ qt/mtpmimedata.h
@@ -0,0 +1,11 @@
+#ifndef MTPMIMEDATA_H
+#define MTPMIMEDATA_H
+
+
+class MtpMimeData : public QMimeData
+{
+public:
+	MtpMimeData();
+};
+
+#endif // MTPMIMEDATA_H
