03939f09464202d5f3e08f7d66dd9f5643228ce0
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-09-12 00:56:24 +0300

    Revert "use tokenizer for completion"
    This reverts commit 9570255c9ecc5e4abf505af347bbd23d0443c113.

--- cli/Session.cpp
+++ cli/Session.cpp
@@ -78,17 +78,14 @@ namespace cli
 
 	char ** Session::CompletionCallback(const char *text, int start, int end)
 	{
-		Tokens tokens;
-		Tokenizer(text, tokens);
-		if (tokens.size() < 2) //0 or 1
+		if (start == 0)
 		{
-			std::string command = !tokens.empty()? tokens.back(): std::string();
 			char **comp = static_cast<char **>(calloc(sizeof(char *), _commands.size() + 1));
 			auto it = _commands.begin();
 			size_t i = 0, n = _commands.size();
 			for(; n--; ++it)
 			{
-				if (end != 0 && it->first.compare(0, end, command) != 0)
+				if (end != 0 && it->first.compare(0, end, text) != 0)
 					continue;
 
 				comp[i++] = strdup(it->first.c_str());
@@ -99,10 +96,6 @@ namespace cli
 				comp = NULL;
 			};
 			return comp;
-		}
-		else
-		{
-			//completion
 		}
 		return NULL;
 	}
