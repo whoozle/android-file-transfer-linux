0c39665a2f6cb1e9c1e582d5e53160f2b785dd8a
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-09-23 09:58:24 +0300

    changed license to gpl v3

--- cli/CommandLine.cpp
+++ cli/CommandLine.cpp
@@ -1,21 +1,22 @@
 /*
- * Android File Transfer for Linux: MTP client for android devices
- * Copyright (C) 2015  Vladimir Menshakov
+    This file is part of Android File Transfer For Linux.
+    Copyright (C) 2015  Vladimir Menshakov
 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+    Android File Transfer For Linux is free software: you can redistribute
+    it and/or modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+    Android File Transfer For Linux is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+    You should have received a copy of the GNU General Public License
+    along with Android File Transfer For Linux.
+    If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include <cli/CommandLine.h>
 
 #include <stdio.h>
