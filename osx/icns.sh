dd60ed61edc4abb1ebdaa0104d81bff999a6dad9
Alexey Gaziev <alex.gaziev@gmail.com>
2015-12-23 19:21:14 +0800

    Prepare osx package build

---
+++ osx/icns.sh
@@ -0,0 +1,19 @@
+export PROJECT=android-file-transfer
+export ICONDIR=./$PROJECT.iconset
+export ORIGICON=./android-file-transfer.png
+
+mkdir $ICONDIR
+
+# Normal screen icons
+for SIZE in 16 32 64 128 256 512; do
+sips -z $SIZE $SIZE $ORIGICON --out $ICONDIR/icon_${SIZE}x${SIZE}.png ;
+done
+
+# Retina display icons
+for SIZE in 32 64 256 512; do
+sips -z $SIZE $SIZE $ORIGICON --out $ICONDIR/icon_$(expr $SIZE / 2)x$(expr $SIZE / 2)x2.png ;
+done
+
+# Make a multi-resolution Icon
+iconutil -c icns -o ./$PROJECT.icns $ICONDIR
+rm -rf $ICONDIR #it is useless now
