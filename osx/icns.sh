75f8b220e5f4c0eddc5c175e6b8d7a8e065d9444
Wandmalfarbe <Wandmalfarbe@users.noreply.github.com>
2017-02-16 20:23:22 +0100

    Created new icon in vector format (SVG). Replaced all Icons with high resolution icons. Modified the icon build script to generate correct high resolution icons.

--- osx/icns.sh
+++ osx/icns.sh
@@ -10,8 +10,8 @@ sips -z $SIZE $SIZE $ORIGICON --out $ICONDIR/icon_${SIZE}x${SIZE}.png ;
 done
 
 # Retina display icons
-for SIZE in 32 64 256 512; do
-sips -z $SIZE $SIZE $ORIGICON --out $ICONDIR/icon_$(expr $SIZE / 2)x$(expr $SIZE / 2)x2.png ;
+for SIZE in 32 64 256 512 1024; do
+sips -z $SIZE $SIZE $ORIGICON --out $ICONDIR/icon_$(expr $SIZE / 2)x$(expr $SIZE / 2)@2x.png ;
 done
 
 # Make a multi-resolution Icon
