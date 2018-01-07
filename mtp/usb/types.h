874c0b551450bdd84febc971bf89464a01beea0b
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-01-07 17:35:17 +0000

    updated year in license

--- mtp/usb/types.h
+++ mtp/usb/types.h
@@ -1,6 +1,6 @@
 /*
     This file is part of Android File Transfer For Linux.
-    Copyright (C) 2015-2017  Vladimir Menshakov
+    Copyright (C) 2015-2018  Vladimir Menshakov
 
     Android File Transfer For Linux is free software: you can redistribute
     it and/or modify it under the terms of the GNU General Public License
