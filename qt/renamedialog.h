3b3abdceed087149cdc9f068fc342df47e6e1384
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-03-12 17:10:42 +0000

    changed license to LGPL v2.1

--- qt/renamedialog.h
+++ qt/renamedialog.h
@@ -2,20 +2,20 @@
     This file is part of Android File Transfer For Linux.
     Copyright (C) 2015-2018  Vladimir Menshakov
 
-    Android File Transfer For Linux is free software: you can redistribute
-    it and/or modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License,
+    or (at your option) any later version.
 
-    Android File Transfer For Linux is distributed in the hope that it will
-    be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    This library is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    Lesser General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Android File Transfer For Linux.
-    If not, see <http://www.gnu.org/licenses/>.
- */
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library; if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
 
 #ifndef RENAMEDIALOG_H
 #define RENAMEDIALOG_H
