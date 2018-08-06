78be1e297326d754740e31e52304d85f3b0b5582
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-08-06 12:48:33 +0100

    implemented fuser-like opened fd scanner

--- mtp/usb/DeviceBusyException.cpp
+++ mtp/usb/DeviceBusyException.cpp
@@ -1,10 +1,103 @@
 #include <mtp/usb/DeviceBusyException.h>
+#include <mtp/log.h>
+#ifdef __linux__
+#	include <fcntl.h>
+#	include <unistd.h>
+#	include <linux/limits.h>
+
+//mtp
+#	include <Exception.h>
+#	include <usb/Directory.h>
+#endif
 
 namespace mtp { namespace usb
 {
+	namespace
+	{
+		std::string ReadLink(const std::string &path)
+		{
+#ifdef __linux__
+			char buf[NAME_MAX];
+			auto pathSize = readlink(path.c_str(), buf, sizeof(buf));
+			if (pathSize >= 0)
+				return std::string(buf, pathSize);
+
+			debug("Readlink ", path, ": ", posix::Exception::GetErrorMessage(errno));
+			return buf;
+#endif
+			return std::string();
+		}
+	}
 
 	DeviceBusyException::DeviceBusyException(int fd, const std::string &msg):
 		std::runtime_error(msg)
-	{ }
+	{
+		try
+		{
+//only linux for now
+#ifdef __linux__
+		do
+		{
+			if (fd < 0)
+				break;
+
+			debug("trying to find processes which are holding fd ", fd);
+#if 0 //apple
+			char filePath[PATH_MAX];
+			if (fcntl(fd, F_GETPATH, filePath) == -1)
+			{
+				error("error getting file path for fd ", fd, ": ", Exception::GetErrorMessage(errno));
+				break;
+			}
+#else
+			std::string filePath = ReadLink("/proc/self/fd/" + std::to_string(fd));
+			if (filePath.empty())
+				break;
+#endif
+			debug("mapped ", fd, " to ", filePath);
+			Directory proc("/proc");
+			while(true)
+			{
+				auto pidPath = proc.Read();
+				if (pidPath.empty())
+					break;
+
+				int pid;
+				if (sscanf(pidPath.c_str(), "%d", &pid) != 1)
+					continue;
+
+				debug("got process dir ", pidPath);
+				auto fdsRoot = "/proc/" + pidPath + "/fd";
+
+				try
+				{
+					Directory fds(fdsRoot);
+					while(true)
+					{
+						auto fdPath = fds.Read();
+						if (fdPath.empty())
+							break;
+
+						auto fdTarget = ReadLink(fdsRoot + "/" + fdPath);
+						if (fdTarget.empty())
+							continue;
+
+						//debug("read mapping to ", fdTarget);
+						if (fdTarget == filePath)
+						{
+							debug("process ", pid, " is holding file descriptor to ", filePath);
+							Processes.push_back(pid);
+						}
+					}
+				}
+				catch(const std::exception & ex)
+				{ debug("error reading ", fdsRoot, ": ", ex.what()); }
+			}
+		}
+		while(false);
+#endif
+		} catch(const std::exception &ex)
+		{ debug("DeviceBusyException error: ", ex.what()); }
+	}
 
 }}
