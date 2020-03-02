5de45af898c4fc34e99aa814616b8845733bc4d6
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-03-02 22:18:21 +0000

    replace class hash with struct

--- mtp/ptp/ObjectId.h
+++ mtp/ptp/ObjectId.h
@@ -55,9 +55,9 @@ namespace mtp
 
 namespace std
 {
-	template<> class hash<mtp::ObjectId>
+	template<> struct hash<mtp::ObjectId>
 	{ public: size_t operator()(const mtp::ObjectId &id) const { return id.Id; } };
-	template<> class hash<mtp::StorageId>
+	template<> struct hash<mtp::StorageId>
 	{ public: size_t operator()(const mtp::StorageId &id) const { return id.Id; } };
 }
 #endif
