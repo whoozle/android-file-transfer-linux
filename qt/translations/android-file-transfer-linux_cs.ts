fa5a3245dd6a3319e95527b3f6048d6154829964
d3im <deim@seznam.cz>
2020-05-27 14:45:31 +0200

    first version of czech translation

---
+++ qt/translations/android-file-transfer-linux_cs.ts
@@ -0,0 +1,495 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>CommandQueue</name>
+    <message>
+        <location filename="../commandqueue.cpp" line="63"/>
+        <source>Loading media library…</source>
+        <translation>Načítání knihovny médií...</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="64"/>
+        <source>Querying artists…</source>
+        <translation>Zjišťuji umělce...</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="65"/>
+        <source>Loading artists…</source>
+        <translation>Načítám umělce...</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="66"/>
+        <source>Querying albums…</source>
+        <translation>Zjišťuji alba...</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="67"/>
+        <source>Loading albums…</source>
+        <translation>Načítám alba...</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="68"/>
+        <source>Done</source>
+        <translation>Hotovo</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="320"/>
+        <source>Setting cover for album %1</source>
+        <translation>Nastavuji obal pro album %1</translation>
+    </message>
+</context>
+<context>
+    <name>CreateDirectoryDialog</name>
+    <message>
+        <location filename="../createdirectorydialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="../createdirectorydialog.ui" line="20"/>
+        <source>Create New Directory</source>
+        <translation>Vytvořit Novou Složku</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Android File Transfer for Linux</source>
+        <translation>Android File Transfer pro Linux</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="65"/>
+        <source>&amp;File</source>
+        <translation>&amp;Soubor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="80"/>
+        <source>&amp;Navigate</source>
+        <translation>&amp;Navigovat</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="114"/>
+        <source>Upload</source>
+        <translation>Nahrát</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="117"/>
+        <source>Ctrl+U</source>
+        <translation>Ctrl+U</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="122"/>
+        <source>&amp;CreateDirectory</source>
+        <translation>&amp;Vytvořit Složku</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="125"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="130"/>
+        <source>UploadDirectory</source>
+        <translation>Nahrrát Složku</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="135"/>
+        <source>&amp;Back</source>
+        <translation>&amp;Zpět</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="138"/>
+        <source>Backspace</source>
+        <translation>Backspace</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="143"/>
+        <source>&amp;Go Down</source>
+        <translation>&amp;Jít Dolů</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="146"/>
+        <source>Return</source>
+        <translation>Zpět</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="151"/>
+        <source>Upload Album</source>
+        <translation>Nahrát Album</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="156"/>
+        <source>&amp;Rename</source>
+        <translation>&amp;Přejmenovat</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="159"/>
+        <source>F2</source>
+        <translation>F2</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="164"/>
+        <source>D&amp;elete</source>
+        <translation>&amp;Smazat</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="167"/>
+        <source>Del</source>
+        <translation>Del</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="172"/>
+        <source>&amp;Download</source>
+        <translation>&amp;Stáhnout</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="175"/>
+        <source>Ctrl+D</source>
+        <translation>Ctrl+D</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="180"/>
+        <source>E&amp;xit</source>
+        <translation>U&amp;končit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="183"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="188"/>
+        <source>Re&amp;fresh</source>
+        <translation>&amp;Obnovit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="191"/>
+        <source>F5</source>
+        <translation>F5</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="196"/>
+        <source>&amp;Paste</source>
+        <translation>&amp;Vložit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="199"/>
+        <source>Ctrl+V</source>
+        <translation>Ctrl+V</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="207"/>
+        <source>Show Thumbnails</source>
+        <translation>Zobrazit Náhledy</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="210"/>
+        <source>Show Thumbnails of images where available</source>
+        <translation>Zobrazit Náhledy obrázků</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="215"/>
+        <location filename="../mainwindow.cpp" line="117"/>
+        <source>Import Music</source>
+        <translation>Importovat Hudbu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="220"/>
+        <source>Remove Cover</source>
+        <translation>Odstranit Obal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="225"/>
+        <location filename="../mainwindow.cpp" line="1034"/>
+        <location filename="../mainwindow.cpp" line="1045"/>
+        <source>Attach Cover</source>
+        <translation>Vložit Obal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="230"/>
+        <source>Import Music Files</source>
+        <translation>Importovat Hudební Soubory</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="233"/>
+        <source>Import individual music files</source>
+        <translation>Importovat jednotlivé hudební soubory</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="173"/>
+        <source>MTPZ Keys Download</source>
+        <translation>Stáhnout MTPZ klíče</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="176"/>
+        <source>Could not download keys, please find the error below:
+
+%1
+
+Please look for .mtpz-data file on the internet and manually install it to your home directory.</source>
+        <translation>Nemohu stáhnout klíče, prosím chybu najdete tady:
+
+%1
+
+Prosím hledejte .mtpz-data soubor na internetu a vložte jej do své domovské složky.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="184"/>
+        <source>Could not write keys to %1</source>
+        <translation>Nemohu zapsat klíče do %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <source>Could not write keys, please find the error below:
+
+%1
+
+Please look for .mtpz-data file on the internet and manually install it to your home directory.</source>
+        <translation>Nemohu zapsat klíče, prosím chybu najdete tady:
+
+%1
+
+Prosím hledejte .mtpz-data soubor na internetu a vložte jej do své domovské složky.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>MTPZ keys have been installed to your system.</source>
+        <translation>MTPZ klíče byly nainstalovány do vašeho systému.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="210"/>
+        <source>Your MTPZ keys failed to install or load.
+
+Please restart the application to try again.
+
+Exception: %1</source>
+        <translation>Instalace nebo načtení Vašich MTPZ klíčů selhala
+
+Prosím restartujte aplikaci pro nový pokus
+
+Vyjímka: %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="253"/>
+        <source>The following processes are keeping file descriptors for your device:
+</source>
+        <translation>Následující procesy drží popisovače souboru pro vaše zařízení:
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="257"/>
+        <source>Device is busy</source>
+        <translation>Zařízení je zaneprázdněno</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="258"/>
+        <source>Device is busy, maybe another process is using it.
+
+</source>
+        <translation>Zařízení je zaneprázdněno, možná jej používá jiný proces.
+
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="260"/>
+        <source>Close other MTP applications and restart Android File Transfer.
+
+Press Abort to kill them or Ignore to try next device.</source>
+        <translation>Zavřete ostatní MTP aplikace a restartujte program.
+
+Zmáčkněte Zrušit pro jejich ukončení nebo Ignorovat pro použití dalšího zařízení.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="284"/>
+        <source>Device::Find failed</source>
+        <translation>Zařízení::Hledání selhalo</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="285"/>
+        <source>MTP device could not be opened at the moment
+
+Failure: %1
+Press Reset to reset them or Ignore to try next device.</source>
+        <translation>MTP zařízení nemohlo být nyní otevřeno
+
+Problém: %1
+Zmáčkněte Reset pro obnovu nebo Ignorovat pro použití dalšího zařízení.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="310"/>
+        <source>No MTP device found</source>
+        <translation>Nenalezeno žádné MTP zařízení</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="328"/>
+        <source>MTPZ Keys are Missing</source>
+        <translation>MTPZ Klíče Nenalezeny</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="329"/>
+        <source>It seems your computer is missing an important bit for talking with MTPZ device: private keys and certificate material from Microsoft.
+This means that you can&apos;t download and upload files from/to such devices.
+
+Microsoft could have released that key material and documentation for MTPZ devices as they are not interested in those anymore.
+
+Because of the legal risks we can&apos;t bundle those keys, even though in some countries it&apos;s lawful to modify things to make them working again, just because you own it.
+
+</source>
+        <translation>Vypadá to, že váš počítač postrádá důležitou součást pro komunikaci s MTPZ zařízením: privítní klíče a certifikát od Microsoftu
+To znamená, že nemůžete stáhovat nebo nahrávat soubory z/do tohoto zařízení.
+
+Microsoft mohl vydat tyto klíčové soubory a dokumentaci pro MTPZ zařízení protože už o to nemají zájem.
+
+Protože je monžost překročení legálních restrikcí, nemůžeme přiložit tyto klíče, přestože v některých zemích je to legální - spravit věci tak, aby byly opět funkční, protože je vlastníte.
+
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="341"/>
+        <source>Alternatively I (as an app) can offer you to download keys from the Internet.
+Can I download keys for you?
+
+(Please press Yes only if all of the above is legal in your country or you just don&apos;t care).</source>
+        <translation>Jako alternativa pro vás mohu stáhnout klíče z Internetu automaticky.
+Stáhnout klíče?
+
+(Zmáčkněte Ano jen v případě, že je řečené u vás legální nebo je vám to jedno).</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="362"/>
+        <source>You can look for .mtpz-data file on the internet, download it and place it in your home directory.</source>
+        <translation>Můžete najít .mtpz-data soubor na internetu a stáhnou jej do vaší domovské složky.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="375"/>
+        <location filename="../mainwindow.cpp" line="394"/>
+        <source>MTP</source>
+        <translation>MTP</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="375"/>
+        <source>MTP device does not respond</source>
+        <translation>MTP zařízení neodpovídá</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="394"/>
+        <source>Could not open MTP session: %1</source>
+        <translation>Nemohu otevřít MTP sezení: %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="428"/>
+        <source>No MTP Storages</source>
+        <translation>Žádné MTP Uložiště</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="429"/>
+        <source>No MTP storage found, your device might be locked.
+Please unlock and press Retry to continue or Abort to exit.</source>
+        <translation>Nenalezeno žádné MTP uložiště, vaše zařízení může být zamčeno.
+Prosím odemčete jej a zmáčknětě Zkust znovu pro pokračování nebo Zrušit pro ukončení.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="466"/>
+        <source>Loading Media Library</source>
+        <translation>Načítání Knihovny Médií</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="582"/>
+        <source>Deleting file(s)</source>
+        <translation>Mazání souboru(ů)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="583"/>
+        <source>Are you sure?</source>
+        <translation>Opravdu?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="670"/>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="671"/>
+        <source>Failed to create new directory:
+</source>
+        <translation>Vytvoření nové složky selhalo:
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="739"/>
+        <source>Download Progress</source>
+        <translation>Postup Stahování</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="942"/>
+        <source>Importing Progress</source>
+        <translation>Postup Importu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="969"/>
+        <source>Overwrite confirmation</source>
+        <translation>Potvrzení přepsání</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="969"/>
+        <source>You are about to overwrite file </source>
+        <translation>Chystáte se přepsat soubor </translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1034"/>
+        <source>Could not open selected file</source>
+        <translation>Nemohu otevřít vybraný soubor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1045"/>
+        <source>Could not attach cover: %1</source>
+        <translation>Nemohu vložit obal: %1</translation>
+    </message>
+</context>
+<context>
+    <name>MtpStoragesModel</name>
+    <message>
+        <location filename="../mtpstoragesmodel.cpp" line="46"/>
+        <source>All storages (BUGS, BEWARE)</source>
+        <translation>Všechna uložiště (POZOR, S CHYBAMI)</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressDialog</name>
+    <message>
+        <location filename="../progressdialog.ui" line="20"/>
+        <source>Upload Progress</source>
+        <translation>Postup Nahrávání</translation>
+    </message>
+    <message>
+        <location filename="../progressdialog.cpp" line="104"/>
+        <source>Speed: %1 Kb/s</source>
+        <translation>Rychlost: %1 Kb/s</translation>
+    </message>
+    <message>
+        <location filename="../progressdialog.cpp" line="106"/>
+        <source>Speed: %1 Mb/s</source>
+        <translation>Rychlost: %1 Mb/s</translation>
+    </message>
+    <message>
+        <location filename="../progressdialog.cpp" line="108"/>
+        <source>Speed: %1 Gb/s</source>
+        <translation>Rychlost: %1 Gb/s</translation>
+    </message>
+</context>
+<context>
+    <name>RenameDialog</name>
+    <message>
+        <location filename="../renamedialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="../renamedialog.ui" line="20"/>
+        <source>Rename Object</source>
+        <translation>Přejmenovat Objekt</translation>
+    </message>
+</context>
+</TS>
