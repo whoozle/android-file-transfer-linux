67b15a8ef8cb4e7e718bd4714931b4f2d9135ba2
Heimen Stoffels <vistausss@outlook.com>
2020-10-26 14:21:18 +0100

    Added Dutch translation

---
+++ qt/translations/android-file-transfer-linux_nl.ts
@@ -0,0 +1,495 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>CommandQueue</name>
+    <message>
+        <location filename="../commandqueue.cpp" line="63"/>
+        <source>Loading media libraryâ¦</source>
+        <translation>Bezig met laden van mediaverzameling…</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="64"/>
+        <source>Querying artistsâ¦</source>
+        <translation>Bezig met zoeken naar artiesten…</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="65"/>
+        <source>Loading artistsâ¦</source>
+        <translation>Bezig met laden van artiesten…</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="66"/>
+        <source>Querying albumsâ¦</source>
+        <translation>Bezig met zoeken naar albums…</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="67"/>
+        <source>Loading albumsâ¦</source>
+        <translation>Bezig met laden van albums…</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="68"/>
+        <source>Done</source>
+        <translation>Voltooid</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="320"/>
+        <source>Setting cover for album %1</source>
+        <translation>Bezig met instellen van hoes bij %1</translation>
+    </message>
+</context>
+<context>
+    <name>CreateDirectoryDialog</name>
+    <message>
+        <location filename="../createdirectorydialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Venster</translation>
+    </message>
+    <message>
+        <location filename="../createdirectorydialog.ui" line="20"/>
+        <source>Create New Directory</source>
+        <translation>Nieuwe map</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Android File Transfer for Linux</source>
+        <translation>Android-bestandsoverdracht voor Linux</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="65"/>
+        <source>&amp;File</source>
+        <translation>&amp;Bestand</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="80"/>
+        <source>&amp;Navigate</source>
+        <translation>&amp;Ga</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="114"/>
+        <source>Upload</source>
+        <translation>Uploaden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="117"/>
+        <source>Ctrl+U</source>
+        <translation>Ctrl+U</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="122"/>
+        <source>&amp;CreateDirectory</source>
+        <translation>Nieuwe &amp;map</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="125"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="130"/>
+        <source>UploadDirectory</source>
+        <translation>Map uploaden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="135"/>
+        <source>&amp;Back</source>
+        <translation>&amp;Terug</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="138"/>
+        <source>Backspace</source>
+        <translation>Backspace</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="143"/>
+        <source>&amp;Go Down</source>
+        <translation>&amp;Ga omlaag</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="146"/>
+        <source>Return</source>
+        <translation>Enter</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="151"/>
+        <source>Upload Album</source>
+        <translation>Album uploaden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="156"/>
+        <source>&amp;Rename</source>
+        <translation>Naam &amp;wijzigen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="159"/>
+        <source>F2</source>
+        <translation>F2</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="164"/>
+        <source>D&amp;elete</source>
+        <translation>Verwijd&amp;eren</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="167"/>
+        <source>Del</source>
+        <translation>Del</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="172"/>
+        <source>&amp;Download</source>
+        <translation>&amp;Downloaden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="175"/>
+        <source>Ctrl+D</source>
+        <translation>Ctrl+D</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="180"/>
+        <source>E&amp;xit</source>
+        <translation>Af&amp;sluiten</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="183"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="188"/>
+        <source>Re&amp;fresh</source>
+        <translation>Her&amp;laden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="191"/>
+        <source>F5</source>
+        <translation>F5</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="196"/>
+        <source>&amp;Paste</source>
+        <translation>&amp;Plakken</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="199"/>
+        <source>Ctrl+V</source>
+        <translation>Ctrl+V</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="207"/>
+        <source>Show Thumbnails</source>
+        <translation>Miniaturen tonen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="210"/>
+        <source>Show Thumbnails of images where available</source>
+        <translation>Toon miniatuurvoorbeelden (indien beschikbaar)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="215"/>
+        <location filename="../mainwindow.cpp" line="117"/>
+        <source>Import Music</source>
+        <translation>Muziek importeren</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="220"/>
+        <source>Remove Cover</source>
+        <translation>Hoes verwijderen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="225"/>
+        <location filename="../mainwindow.cpp" line="1034"/>
+        <location filename="../mainwindow.cpp" line="1045"/>
+        <source>Attach Cover</source>
+        <translation>Hoes bijsluiten</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="230"/>
+        <source>Import Music Files</source>
+        <translation>Muziekbestanden importeren</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="233"/>
+        <source>Import individual music files</source>
+        <translation>Importeer losse muziekbestanden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="173"/>
+        <source>MTPZ Keys Download</source>
+        <translation>MTPZ-sleutels downloaden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="176"/>
+        <source>Could not download keys, please find the error below:
+
+%1
+
+Please look for .mtpz-data file on the internet and manually install it to your home directory.</source>
+        <translation>De sleutels kunnen niet worden opgehaald vanwege de volgende fout:
+
+%1
+
+Zoek handmatig naar .mtpz-gegevensbestanden op het internet en plaats ze in je persoonlijke map.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="184"/>
+        <source>Could not write keys to %1</source>
+        <translation>De sleutels kunnen niet worden weggeschreven naar %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <source>Could not write keys, please find the error below:
+
+%1
+
+Please look for .mtpz-data file on the internet and manually install it to your home directory.</source>
+        <translation>De sleutels kunnen niet worden weggeschreven vanwege de volgende fout:
+
+%1
+
+Zoek handmatig naar .mtpz-gegevensbestanden op het internet en plaats ze in je persoonlijke map.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>MTPZ keys have been installed to your system.</source>
+        <translation>De mtpz-sleutels zijn op je systeem geplaatst.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="210"/>
+        <source>Your MTPZ keys failed to install or load.
+
+Please restart the application to try again.
+
+Exception: %1</source>
+        <translation>Je mtpz-sleutels kunnen niet worden geplaatst of geladen.
+
+Herstart het programma en probeer het opnieuw.
+
+Uitzonderingsfout: %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="253"/>
+        <source>The following processes are keeping file descriptors for your device:
+</source>
+        <translation>De volgende processen bevatten bestandsomschrijvingen van je apparaat:
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="257"/>
+        <source>Device is busy</source>
+        <translation>Apparaat is bezig</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="258"/>
+        <source>Device is busy, maybe another process is using it.
+
+</source>
+        <translation>Het apparaat is bezig. Mogelijk is het in gebruik door een ander proces.
+
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="260"/>
+        <source>Close other MTP applications and restart Android File Transfer.
+
+Press Abort to kill them or Ignore to try next device.</source>
+        <translation>Sluit andere mtp-programma&apos;s en herstart Android-bestandsoverdracht.
+
+Druk op &apos;Afbreken&apos; om ze af te sluiten of &apos;Negeren&apos; om een ander apparaat te proberen.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="284"/>
+        <source>Device::Find failed</source>
+        <translation>Apparaat niet aangetroffen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="285"/>
+        <source>MTP device could not be opened at the moment
+
+Failure: %1
+Press Reset to reset them or Ignore to try next device.</source>
+        <translation>Het mtp-apparaat kan momenteel niet worden benaderd.
+
+Foutmelding: %1
+Druk op &apos;Herstellen&apos; om te herstellen of &apos;Negeren&apos; om een ander apparaat te proberen.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="310"/>
+        <source>No MTP device found</source>
+        <translation>Geen mtp-apparaat aangetroffen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="328"/>
+        <source>MTPZ Keys are Missing</source>
+        <translation>De mtpz-sleutels ontbreken</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="329"/>
+        <source>It seems your computer is missing an important bit for talking with MTPZ device: private keys and certificate material from Microsoft.
+This means that you can&apos;t download and upload files from/to such devices.
+
+Microsoft could have released that key material and documentation for MTPZ devices as they are not interested in those anymore.
+
+Because of the legal risks we can&apos;t bundle those keys, even though in some countries it&apos;s lawful to modify things to make them working again, just because you own it.
+
+</source>
+        <translation>Het lijkt er op dat je computer een belangrijk onderdeel mist voor het communiceren met je mtpz-apparaat: privésleutels en certificaatmateriaal van Microsoft.
+Dit houdt in dat je geen bestanden kunt downloaden en uploaden van/naar zulke apparaten.
+
+Microsoft had dit materiaal beschikbaar kunnen stellen aangezien ze niet meer geïnteresseerd zijn in deze apparaten.
+
+Vanwege juridische conflicten kunnen deze sleutels niet meeleveren, zelfs al is dat in jouw land mogelijk legaal.
+
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="341"/>
+        <source>Alternatively I (as an app) can offer you to download keys from the Internet.
+Can I download keys for you?
+
+(Please press Yes only if all of the above is legal in your country or you just don&apos;t care).</source>
+        <translation>Het enige wat ik je kan bieden is de mogelijkheid deze sleutels te downloaden van het internet.
+Zal ik dat voor je doen?
+
+(klik alleen op &apos;Ja&apos; als dit legaal is in je land of als de wet je niet interesseert)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="362"/>
+        <source>You can look for .mtpz-data file on the internet, download it and place it in your home directory.</source>
+        <translation>Je kunt .mtpz-gegevens vinden op het internet en deze in je persoonlijke map plaatsen.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="375"/>
+        <location filename="../mainwindow.cpp" line="394"/>
+        <source>MTP</source>
+        <translation>MTP</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="375"/>
+        <source>MTP device does not respond</source>
+        <translation>MTP-apparaat reageert niet</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="394"/>
+        <source>Could not open MTP session: %1</source>
+        <translation>De mtp-sessie kan niet worden geopend: %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="428"/>
+        <source>No MTP Storages</source>
+        <translation>Geen mtp-apparaten</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="429"/>
+        <source>No MTP storage found, your device might be locked.
+Please unlock and press Retry to continue or Abort to exit.</source>
+        <translation>Geen mtp-apparaat aangetroffen - wellicht is je apparaat vergrendeld.
+Ontgrendel je apparaat en druk op &apos;Opnieuw&apos; of &apos;Afbreken&apos;.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="466"/>
+        <source>Loading Media Library</source>
+        <translation>Bezig met laden van mediaverzameling</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="582"/>
+        <source>Deleting file(s)</source>
+        <translation>Bezig met verwijderen van bestand(en)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="583"/>
+        <source>Are you sure?</source>
+        <translation>Weet je het zeker?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="670"/>
+        <source>Error</source>
+        <translation>Fout</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="671"/>
+        <source>Failed to create new directory:
+</source>
+        <translation>De map kan niet worden aangemaakt:
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="739"/>
+        <source>Download Progress</source>
+        <translation>Downloadvoortgang</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="942"/>
+        <source>Importing Progress</source>
+        <translation>Importvoortgang</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="969"/>
+        <source>Overwrite confirmation</source>
+        <translation>Overschrijven bevestigen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="969"/>
+        <source>You are about to overwrite file </source>
+        <translation>Je staat op het punt om het volgende te overschrijven:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1034"/>
+        <source>Could not open selected file</source>
+        <translation>Kan het geselecteerde bestand niet openen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1045"/>
+        <source>Could not attach cover: %1</source>
+        <translation>De hoes kan niet worden bijgesloten: %1</translation>
+    </message>
+</context>
+<context>
+    <name>MtpStoragesModel</name>
+    <message>
+        <location filename="../mtpstoragesmodel.cpp" line="46"/>
+        <source>All storages (BUGS, BEWARE)</source>
+        <translation>Alle opslagapparaten (LET OP: MOGELIJK ONBETROUWBAAR)</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressDialog</name>
+    <message>
+        <location filename="../progressdialog.ui" line="20"/>
+        <source>Upload Progress</source>
+        <translation>Uploadvoortgang</translation>
+    </message>
+    <message>
+        <location filename="../progressdialog.cpp" line="104"/>
+        <source>Speed: %1 kB/s</source>
+        <translation>Snelheid: %1 kB/s</translation>
+    </message>
+    <message>
+        <location filename="../progressdialog.cpp" line="106"/>
+        <source>Speed: %1 MB/s</source>
+        <translation>Snelheid: %1 MB/s</translation>
+    </message>
+    <message>
+        <location filename="../progressdialog.cpp" line="108"/>
+        <source>Speed: %1 GB/s</source>
+        <translation>Snelheid: %1 GB/s</translation>
+    </message>
+</context>
+<context>
+    <name>RenameDialog</name>
+    <message>
+        <location filename="../renamedialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Venster</translation>
+    </message>
+    <message>
+        <location filename="../renamedialog.ui" line="20"/>
+        <source>Rename Object</source>
+        <translation>Itemnaam wijzigen</translation>
+    </message>
+</context>
+</TS>
