a155c872793dc8fba4b7d47ff17756d5ab404113
WhiredPlanck <fungdaat31@outlook.com>
2021-01-13 13:14:21 +0800

    translation: Add Chinese Simplified translation

---
+++ qt/translations/android-file-transfer-linux_zh-CN.ts
@@ -0,0 +1,502 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>CommandQueue</name>
+    <message>
+        <location filename="../commandqueue.cpp" line="63"/>
+        <source>Loading media library…</source>
+        <translation>正在加载媒体库……</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="64"/>
+        <source>Querying artists…</source>
+        <translation>正在查询艺术家……</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="65"/>
+        <source>Loading artists…</source>
+        <translation>正在加载艺术家……</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="66"/>
+        <source>Querying albums…</source>
+        <translation>正在查询相册……</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="67"/>
+        <source>Loading albums…</source>
+        <translation>正在加载相册……</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="68"/>
+        <source>Done</source>
+        <translation>已完成</translation>
+    </message>
+    <message>
+        <location filename="../commandqueue.cpp" line="320"/>
+        <source>Setting cover for album %1</source>
+        <translation>为相册 %1 设定封面</translation>
+    </message>
+</context>
+<context>
+    <name>CreateDirectoryDialog</name>
+    <message>
+        <location filename="../createdirectorydialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>新目录</translation>
+    </message>
+    <message>
+        <location filename="../createdirectorydialog.ui" line="20"/>
+        <source>Create New Directory</source>
+        <translation>创建新目录</translation>
+    </message>
+</context>
+<context>
+    <name>DevicesDialog</name>
+    <message>
+        <location filename="../devicesdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>设备行为</translation>
+    </message>
+    <message>
+        <location filename="../devicesdialog.cpp" line="18"/>
+        <source>Rescan Devices</source>
+        <translation>重新扫描设备</translation>
+    </message>
+    <message>
+        <location filename="../devicesdialog.cpp" line="19"/>
+        <source>Kill Users</source>
+        <translation>杀死用户</translation>
+    </message>
+    <message>
+        <location filename="../devicesdialog.cpp" line="52"/>
+        <source>The following processes are keeping file descriptors for your device:
+</source>
+        <translation>以下进程正在为您的设备保存文件描述符：
+</translation>
+    </message>
+    <message>
+        <location filename="../devicesdialog.cpp" line="56"/>
+        <source>Device is busy</source>
+        <translation>设备正忙</translation>
+    </message>
+    <message>
+        <location filename="../devicesdialog.cpp" line="57"/>
+        <source>Device is busy, maybe another process is using it.
+
+</source>
+        <translation>设备正忙，可能有另外的进程正在使用。
+
+</translation>
+    </message>
+    <message>
+        <location filename="../devicesdialog.cpp" line="59"/>
+        <source>Close other MTP applications and restart Android File Transfer.
+
+Press Abort to kill them or Ignore to try next device.</source>
+        <translation>请关闭其他的 MTP 应用并重新启动 Android 文件传输器。
+
+点击“放弃”以终止它们，或“忽略”以尝试下一台设备。</translation>
+    </message>
+    <message>
+        <location filename="../devicesdialog.cpp" line="129"/>
+        <source>USB Device %1:%2</source>
+        <translation>USB 设备 %1:%2</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Android File Transfer for Linux</source>
+        <translation>Android 文件传输器（Linux 版）</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="65"/>
+        <source>&amp;File</source>
+        <translation>文件(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="80"/>
+        <source>&amp;Navigate</source>
+        <translation>导航(&amp;N)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="114"/>
+        <source>Upload</source>
+        <translation>上传</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="117"/>
+        <source>Ctrl+U</source>
+        <translation>Ctrl+U</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="122"/>
+        <source>&amp;CreateDirectory</source>
+        <translation>创建目录(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="125"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="130"/>
+        <source>UploadDirectory</source>
+        <translation>上传目录</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="135"/>
+        <source>&amp;Back</source>
+        <translation>后退(&amp;B)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="138"/>
+        <source>Backspace</source>
+        <translation>Backspace</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="143"/>
+        <source>&amp;Go Down</source>
+        <translation>转入(&amp;G)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="146"/>
+        <source>Return</source>
+        <translation>Return</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="151"/>
+        <source>Upload Album</source>
+        <translation>上传相册</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="156"/>
+        <source>&amp;Rename</source>
+        <translation>重命名(&amp;R)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="159"/>
+        <source>F2</source>
+        <translation>F2</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="164"/>
+        <source>D&amp;elete</source>
+        <translation>删除(&amp;E)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="167"/>
+        <source>Del</source>
+        <translation>Del</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="172"/>
+        <source>&amp;Download</source>
+        <translation>下载(&amp;D)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="175"/>
+        <source>Ctrl+D</source>
+        <translation>Ctrl+D</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="180"/>
+        <source>E&amp;xit</source>
+        <translation>退出(&amp;X)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="183"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="188"/>
+        <source>Re&amp;fresh</source>
+        <translation>刷新(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="191"/>
+        <source>F5</source>
+        <translation>F5</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="196"/>
+        <source>&amp;Paste</source>
+        <translation>粘贴(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="199"/>
+        <source>Ctrl+V</source>
+        <translation>Ctrl+V</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="207"/>
+        <source>Show Thumbnails</source>
+        <translation>显示缩略图</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="210"/>
+        <source>Show Thumbnails of images where available</source>
+        <translation>显示图像的缩略图（如果可用）</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="215"/>
+        <location filename="../mainwindow.cpp" line="117"/>
+        <source>Import Music</source>
+        <translation>导入音乐</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="220"/>
+        <source>Remove Cover</source>
+        <translation>移除封面</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="225"/>
+        <location filename="../mainwindow.cpp" line="971"/>
+        <location filename="../mainwindow.cpp" line="982"/>
+        <source>Attach Cover</source>
+        <translation>附加封面</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="230"/>
+        <source>Import Music Files</source>
+        <translation>导入音乐文件</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="233"/>
+        <source>Import individual music files</source>
+        <translation>导入个人音乐文件</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="173"/>
+        <source>MTPZ Keys Download</source>
+        <translation>MTPZ 密钥下载</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="176"/>
+        <source>Could not download keys, please find the error below:
+
+%1
+
+Please look for .mtpz-data file on the internet and manually install it to your home directory.</source>
+        <translation>无法下载密钥，请在下方找出错误：
+
+%1
+
+请在互联网上寻找 .mtpz-data 文件并手动将其安装到家目录中。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="184"/>
+        <source>Could not write keys to %1</source>
+        <translation>无法将密钥写入 %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <source>Could not write keys, please find the error below:
+
+%1
+
+Please look for .mtpz-data file on the internet and manually install it to your home directory.</source>
+        <translation>无法写入密钥，请在下方找出错误：
+
+%1
+
+请在互联网上寻找 .mtpz-data 文件并手动将其安装到家目录中。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>MTPZ keys have been installed to your system.</source>
+        <translation>MTPZ 密钥已安装到系统。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="210"/>
+        <source>Your MTPZ keys failed to install or load.
+
+Please restart the application to try again.
+
+Exception: %1</source>
+        <translation>MTPZ 密钥安装或加载失败。
+
+请重启应用再试一次。
+
+异常：%1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="241"/>
+        <source>No MTP device found</source>
+        <translation>找不到 MTP 设备</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="259"/>
+        <source>MTPZ Keys are Missing</source>
+        <translation>MTPZ 密钥缺失</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="260"/>
+        <source>It seems your computer is missing an important bit for talking with MTPZ device: private keys and certificate material from Microsoft.
+This means that you can&apos;t download and upload files from/to such devices.
+
+Microsoft could have released that key material and documentation for MTPZ devices as they are not interested in those anymore.
+
+Because of the legal risks we can&apos;t bundle those keys, even though in some countries it&apos;s lawful to modify things to make them working again, just because you own it.
+
+</source>
+        <translation>计算机似乎缺少了与MTPZ设备对话的重要部分：来自微软（Microsoft）的私钥和证书材料。
+这意味着无法从这些设备下载和上传文件。
+
+微软可能已经发布了 MTPZ 设备的密钥材料和文档，因为他们对这些不再感兴趣了。
+
+尽管在一些国家，因个人持有文件而对其进行修改、使其重新工作的行为是合法的，但由于各种法律风险，我们无法捆绑这些密钥。
+
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="272"/>
+        <source>Alternatively I (as an app) can offer you to download keys from the Internet.
+Can I download keys for you?
+
+(Please press Yes only if all of the above is legal in your country or you just don&apos;t care).</source>
+        <translation>此外，本应用可为您从互联网上下载密钥。
+可否下载？
+
+（仅当上述行为在您所在国家合法，或者您不在意时，才点击“是”）。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="293"/>
+        <source>You can look for .mtpz-data file on the internet, download it and place it in your home directory.</source>
+        <translation>可在互联网上寻找 .mtpz-data 文件，下载并将其放入家目录中。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="306"/>
+        <location filename="../mainwindow.cpp" line="325"/>
+        <source>MTP</source>
+        <translation>MTP</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="306"/>
+        <source>MTP device does not respond</source>
+        <translation>MTP 设备没有回应</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="325"/>
+        <source>Could not open MTP session: %1</source>
+        <translation>无法打开 MTP 会话：%1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="359"/>
+        <source>No MTP Storages</source>
+        <translation>没有 MTP 存储空间</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="360"/>
+        <source>No MTP storage found, your device might be locked.
+Please unlock and press Retry to continue or Abort to exit.</source>
+        <translation>找不到 MTP 存储空间，您的设备可能已被锁定。
+请解锁设备并点击“重试”以继续或“放弃”以退出。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="403"/>
+        <source>Loading Media Library</source>
+        <translation>正在加载媒体库</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="519"/>
+        <source>Deleting file(s)</source>
+        <translation>正在删除文件</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="520"/>
+        <source>Are you sure?</source>
+        <translation>确定吗？</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="607"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="608"/>
+        <source>Failed to create new directory:
+</source>
+        <translation>创建新目录失败：
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="676"/>
+        <source>Download Progress</source>
+        <translation>下载进程</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="879"/>
+        <source>Importing Progress</source>
+        <translation>导入进程</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="906"/>
+        <source>Overwrite confirmation</source>
+        <translation>覆盖确认</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="906"/>
+        <source>You are about to overwrite file </source>
+        <translation>即将覆盖文件 </translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="971"/>
+        <source>Could not open selected file</source>
+        <translation>无法打开选定文件</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="982"/>
+        <source>Could not attach cover: %1</source>
+        <translation>无法附加封面：%1</translation>
+    </message>
+</context>
+<context>
+    <name>MtpStoragesModel</name>
+    <message>
+        <location filename="../mtpstoragesmodel.cpp" line="46"/>
+        <source>All storages (BUGS, BEWARE)</source>
+        <translation>所有存储空间 (有 Bug，请留意)</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressDialog</name>
+    <message>
+        <location filename="../progressdialog.ui" line="20"/>
+        <source>Upload Progress</source>
+        <translation>上传进程</translation>
+    </message>
+    <message>
+        <location filename="../progressdialog.cpp" line="104"/>
+        <source>Speed: %1 kB/s</source>
+        <translation>速率：%1 kB/s</translation>
+    </message>
+    <message>
+        <location filename="../progressdialog.cpp" line="106"/>
+        <source>Speed: %1 MB/s</source>
+        <translation>速率：%1 MB/s</translation>
+    </message>
+    <message>
+        <location filename="../progressdialog.cpp" line="108"/>
+        <source>Speed: %1 GB/s</source>
+        <translation>速率：%1 GB/s</translation>
+    </message>
+</context>
+<context>
+    <name>RenameDialog</name>
+    <message>
+        <location filename="../renamedialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>重命名</translation>
+    </message>
+    <message>
+        <location filename="../renamedialog.ui" line="20"/>
+        <source>Rename Object</source>
+        <translation>重命名对象</translation>
+    </message>
+</context>
+</TS>
